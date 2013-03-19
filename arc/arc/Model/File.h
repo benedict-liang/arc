d15ce63c8b95fece89862624913e7ba4145daad3
Jerome Cheng <jcheng31@gmail.com>
2013-03-19 17:53:13 +0800

    Add File.

---
+++ arc/arc/Model/File.h
@@ -0,0 +1,16 @@
+//
+//  File.h
+//  arc
+//
+//  Created by Jerome Cheng on 19/3/13.
+//  Copyright (c) 2013 nus.cs3217. All rights reserved.
+//
+
+#import "FileObject.h"
+
+@interface File : FileObject
+
+// Returns an NSString containing the full contents of this file.
+- (NSString*) getContents;
+
+@end
---
+++ arc/arc/Model/File.m
@@ -0,0 +1,13 @@
+//
+//  File.m
+//  arc
+//
+//  Created by Jerome Cheng on 19/3/13.
+//  Copyright (c) 2013 nus.cs3217. All rights reserved.
+//
+
+#import "File.h"
+
+@implementation File
+
+@end
