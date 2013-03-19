ec2d6755fe51a9ad744faee1de5e831c97876a79
Benedict Liang <benedictljj@gmail.com>
2013-03-19 21:45:19 +0800

    added CodeViewController

---
+++ arc/arc/View Controllers/CodeViewController.h
@@ -0,0 +1,13 @@
+//
+//  CodeViewController.h
+//  arc
+//
+//  Created by Benedict Liang on 19/3/13.
+//  Copyright (c) 2013 nus.cs3217. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface CodeViewController : UIViewController
+
+@end
---
+++ arc/arc/View Controllers/CodeViewController.m
@@ -0,0 +1,38 @@
+//
+//  CodeViewController.m
+//  arc
+//
+//  Created by Benedict Liang on 19/3/13.
+//  Copyright (c) 2013 nus.cs3217. All rights reserved.
+//
+
+#import "CodeViewController.h"
+
+@interface CodeViewController ()
+
+@end
+
+@implementation CodeViewController
+
+- (id)initWithNibName:(NSString *)nibNameOrNil bundle:(NSBundle *)nibBundleOrNil
+{
+    self = [super initWithNibName:nibNameOrNil bundle:nibBundleOrNil];
+    if (self) {
+        // Custom initialization
+    }
+    return self;
+}
+
+- (void)viewDidLoad
+{
+    [super viewDidLoad];
+	// Do any additional setup after loading the view.
+}
+
+- (void)didReceiveMemoryWarning
+{
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
