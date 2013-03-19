0309c795ebac3ba4edd4133e518f271048067ee8
Benedict Liang <benedictljj@gmail.com>
2013-03-19 17:30:37 +0800

    arc v2

---
+++ arc2/arc2/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  arc2
+//
+//  Created by Benedict Liang on 19/3/13.
+//  Copyright (c) 2013 Benedict Liang. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "AppDelegate.h"
+
+int main(int argc, char *argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
