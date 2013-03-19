3dce6c47aeb72cf5949532335748447d9d298acb
Omer Iqbal <momeriqbal2@gmail.com>
2013-03-19 20:12:02 +0800

    added stub for UIFileNavigationController

---
+++ arc/arc/View Controllers/UIFileNavigationController.h
@@ -0,0 +1,15 @@
+//
+//  UIFileNavigationController.h
+//  arc
+//
+//  Created by omer iqbal on 19/3/13.
+//  Copyright (c) 2013 nus.cs3217. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface UIFileNavigationController : UITableViewController
+
+-(id)initWithFiles:(NSArray*)files;
+
+@end
