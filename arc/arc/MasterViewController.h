dd5787510d6cc2c6ebf2c5c8273f1f24b10c47a3
Benedict Liang <benedictljj@gmail.com>
2013-03-19 17:12:48 +0800

    added project skeleton

---
+++ arc/arc/MasterViewController.h
@@ -0,0 +1,22 @@
+//
+//  MasterViewController.h
+//  arc
+//
+//  Created by Benedict Liang on 19/3/13.
+//  Copyright (c) 2013 Benedict Liang. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@class DetailViewController;
+
+#import <CoreData/CoreData.h>
+
+@interface MasterViewController : UITableViewController <NSFetchedResultsControllerDelegate>
+
+@property (strong, nonatomic) DetailViewController *detailViewController;
+
+@property (strong, nonatomic) NSFetchedResultsController *fetchedResultsController;
+@property (strong, nonatomic) NSManagedObjectContext *managedObjectContext;
+
+@end
