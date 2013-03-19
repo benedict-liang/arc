50d6f9b42988c95f31336057d3782edfdb328467
Benedict Liang <benedictljj@gmail.com>
2013-03-19 21:15:13 +0800

    Modified MainViewController

---
+++ arc/arc/ApplicationState.h
@@ -0,0 +1,13 @@
+//
+//  ApplicationState.h
+//  arc
+//
+//  Created by Benedict Liang on 19/3/13.
+//  Copyright (c) 2013 nus.cs3217. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface ApplicationState : NSObject
+
+@end
---
+++ arc/arc/ApplicationState.m
@@ -0,0 +1,13 @@
+//
+//  ApplicationState.m
+//  arc
+//
+//  Created by Benedict Liang on 19/3/13.
+//  Copyright (c) 2013 nus.cs3217. All rights reserved.
+//
+
+#import "ApplicationState.h"
+
+@implementation ApplicationState
+
+@end
