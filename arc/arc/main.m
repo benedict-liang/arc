c7e3da08aa66bc9929230ce0cc4a2b4beae938ba
Michael Yong <wrong92@gmail.com>
2013-03-19 17:44:19 +0800

    new project

---
+++ arc/arc/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  arc
+//
+//  Created by Yong Michael on 19/3/13.
+//  Copyright (c) 2013 nus.cs3217. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "AppDelegate.h"
+
+int main(int argc, char *argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
