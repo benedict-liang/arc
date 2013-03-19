57b1589de7d380441a26a1947a61200653797789
Jerome Cheng <jcheng31@gmail.com>
2013-03-19 17:30:15 +0800

    Add FileObject.

---
+++ arc/arc/FileObject.h
@@ -0,0 +1,25 @@
+//
+//  FileObject.h
+//  arc
+//
+//  Created by Jerome Cheng on 19/3/13.
+//  Copyright (c) 2013 Benedict Liang. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface FileObject : NSObject
+
+// The name of this object.
+@property (strong, nonatomic) NSString* name;
+
+// The full file path of this object.
+@property (strong, nonatomic) NSString* path;
+
+// The parent of this object (if any.)
+@property (strong, nonatomic) FileObject* parent;
+
+// Whether this object is a folder or not.
+@property bool isFolder;
+
+@end
---
+++ arc/arc/FileObject.m
@@ -0,0 +1,13 @@
+//
+//  FileObject.m
+//  arc
+//
+//  Created by Jerome Cheng on 19/3/13.
+//  Copyright (c) 2013 Benedict Liang. All rights reserved.
+//
+
+#import "FileObject.h"
+
+@implementation FileObject
+
+@end
