7275d10e56b4fca836090427b769463aedb10543
Jerome Cheng <jcheng31@gmail.com>
2013-03-19 17:53:23 +0800

    Add Folder.

---
+++ arc/arc/Model/Folder.m
@@ -0,0 +1,13 @@
+//
+//  Folder.m
+//  arc
+//
+//  Created by Jerome Cheng on 19/3/13.
+//  Copyright (c) 2013 nus.cs3217. All rights reserved.
+//
+
+#import "Folder.h"
+
+@implementation Folder
+
+@end
