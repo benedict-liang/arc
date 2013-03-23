9573857bb573f1b044910252394aba88400b9e81
Michael Yong <wrong92@gmail.com>
2013-03-23 22:20:31 +0800

    Added Test Harness.
    TODO: move random test cases into test harness.

---
+++ arc/arcTests/arcTests.h
@@ -0,0 +1,13 @@
+//
+//  arcTests.h
+//  arcTests
+//
+//  Created by Yong Michael on 23/3/13.
+//  Copyright (c) 2013 nus.cs3217. All rights reserved.
+//
+
+#import <SenTestingKit/SenTestingKit.h>
+
+@interface arcTests : SenTestCase
+
+@end
---
+++ arc/arcTests/arcTests.m
@@ -0,0 +1,32 @@
+//
+//  arcTests.m
+//  arcTests
+//
+//  Created by Yong Michael on 23/3/13.
+//  Copyright (c) 2013 nus.cs3217. All rights reserved.
+//
+
+#import "arcTests.h"
+
+@implementation arcTests
+
+- (void)setUp
+{
+    [super setUp];
+    
+    // Set-up code here.
+}
+
+- (void)tearDown
+{
+    // Tear-down code here.
+    
+    [super tearDown];
+}
+
+- (void)test
+{
+    STAssertNotNil(@"asdf", @"This is the test case description");
+}
+
+@end
