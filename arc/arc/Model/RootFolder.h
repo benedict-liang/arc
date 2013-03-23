5b27431f18988e212bfe8a47fa8a2c5efcc161bf
Jerome Cheng <jcheng31@gmail.com>
2013-03-23 14:29:49 +0800

    Add RootFolder.

---
+++ arc/arc/Model/RootFolder.h
@@ -0,0 +1,13 @@
+//
+//  RootFolder.h
+//  arc
+//
+//  Created by Jerome Cheng on 23/3/13.
+//  Copyright (c) 2013 nus.cs3217. All rights reserved.
+//
+
+#import "Folder.h"
+
+@interface RootFolder : Folder
+
+@end
---
+++ arc/arc/Model/RootFolder.m
@@ -0,0 +1,13 @@
+//
+//  RootFolder.m
+//  arc
+//
+//  Created by Jerome Cheng on 23/3/13.
+//  Copyright (c) 2013 nus.cs3217. All rights reserved.
+//
+
+#import "RootFolder.h"
+
+@implementation RootFolder
+
+@end
