7a18c5134e35e4d03cbc6ededf3775bb43fdc0ec
Omer Iqbal <momeriqbal2@gmail.com>
2013-03-25 01:49:16 +0800

    moved imports to MainViewController.h as per standard

--- arc/arc/Controllers/MainViewController.m
+++ arc/arc/Controllers/MainViewController.m
@@ -6,14 +6,8 @@
 //  Copyright (c) 2013 nus.cs3217. All rights reserved.
 //
 
+
 #import "MainViewController.h"
-#import "ApplicationState.h"
-#import "RootFolder.h"
-#import "Folder.h"
-#import "File.h"
-#import "FileNavigationViewController.h"
-#import "CodeViewController.h"
-#import "Constants.h"
 
 @interface MainViewController ()
 @property CodeViewController *codeViewController;
