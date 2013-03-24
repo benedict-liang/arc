c67b45f9eabeeb677dcc803148d52e550d0856ed
Jerome Cheng <jcheng31@gmail.com>
2013-03-24 18:20:56 +0800

    Add FileHelper.
    Update xcodeproj.

---
+++ arc/arc/Model/FileHelper.h
@@ -0,0 +1,13 @@
+//
+//  FileHelper.h
+//  arc
+//
+//  Created by Jerome Cheng on 24/3/13.
+//  Copyright (c) 2013 nus.cs3217. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface FileHelper : NSObject
+
+@end
---
+++ arc/arc/Model/FileHelper.m
@@ -0,0 +1,13 @@
+//
+//  FileHelper.m
+//  arc
+//
+//  Created by Jerome Cheng on 24/3/13.
+//  Copyright (c) 2013 nus.cs3217. All rights reserved.
+//
+
+#import "FileHelper.h"
+
+@implementation FileHelper
+
+@end
