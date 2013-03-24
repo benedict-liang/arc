6fd6f03d64b828032e878728e41ca005cc479b07
Jerome Cheng <jcheng31@gmail.com>
2013-03-24 19:32:55 +0800

    Implement fileWithURL:sourceApplication:annotation:

--- arc/arc/Model/FileHelper.h
+++ arc/arc/Model/FileHelper.h
@@ -10,12 +10,12 @@
 #import "File.h"
 #import "Folder.h"
 #import "FileObject.h"
+#import "RootFolder.h"
 
 @interface FileHelper : NSObject
 
 // Used to handle files passed in from other applications through iOS' "Open in..."
-// Moves the file into a folder named after that application (inside External Applications),
-// then returns it.
+// Moves the file into External Applications, then returns it.
 + (File*)fileWithURL:(NSURL*)url sourceApplication:(NSString*)application annotation:(id)annotation;
 
 @end
--- arc/arc/Model/FileHelper.m
+++ arc/arc/Model/FileHelper.m
@@ -7,7 +7,40 @@
 //
 
 #import "FileHelper.h"
+#import "Constants.h"
 
 @implementation FileHelper
+
+// Used to handle files passed in from other applications through iOS' "Open in..."
+// Moves the file into External Applications, then returns it.
++ (File*)fileWithURL:(NSURL *)url sourceApplication:(NSString *)application annotation:(id)annotation
+{
+    // Get the "Inbox" folder where it's stored.
+    RootFolder *root = [RootFolder getInstance];
+    NSURL *inboxURL = [url URLByDeletingLastPathComponent];
+    Folder *inboxFolder = [[Folder alloc] initWithURL:inboxURL parent:root];
+    
+    // Get the External Applications folder.
+    NSArray *rootContents = [root getContents];
+    Folder *externalFolder;
+    for (FileObject *currentObject in rootContents) {
+        if ([currentObject isKindOfClass:[Folder class]] && [[currentObject name] isEqualToString:FOLDER_EXTERNAL_APPLICATIONS]) {
+            externalFolder = (Folder*)currentObject;
+            break;
+        }
+    }
+    
+    // Create a File object for the received file.
+    File *receivedDocument = [[File alloc] initWithURL:url parent:inboxFolder];
+    
+    // Move it to External Applications.
+    BOOL isMoveSuccessful = [externalFolder takeFile:receivedDocument];
+    if (isMoveSuccessful) {
+        // Get rid of the Inbox folder.
+        [inboxFolder remove];
+        return receivedDocument;
+    }
+    return nil;
+}
 
 @end
