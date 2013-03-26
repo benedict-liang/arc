751db8e02b9f888ad3519b73dfa5ad3c379d1a01
Benedict Liang <benedictljj@gmail.com>
2013-03-26 12:56:36 +0800

    Added TMBundleSyntaxParser.
    All source files that process Textmate Bundles should start with TMBundle.

---
+++ arc/arc/General/TMBundleProcessing/TMBundleSyntaxParser.h
@@ -0,0 +1,13 @@
+//
+//  TMBundleSyntaxParser.h
+//  arc
+//
+//  Created by Benedict Liang on 26/3/13.
+//  Copyright (c) 2013 nus.cs3217. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface TMBundleSyntaxParser : NSObject
+
+@end
---
+++ arc/arc/General/TMBundleProcessing/TMBundleSyntaxParser.m
@@ -0,0 +1,13 @@
+//
+//  TMBundleSyntaxParser.m
+//  arc
+//
+//  Created by Benedict Liang on 26/3/13.
+//  Copyright (c) 2013 nus.cs3217. All rights reserved.
+//
+
+#import "TMBundleSyntaxParser.h"
+
+@implementation TMBundleSyntaxParser
+
+@end
