bba0038573a1a3a1a1b6e2ed877ef830eea05157
Benedict Liang <benedictljj@gmail.com>
2013-03-26 14:26:47 +0800

    Added TMBundleHeader.
    This stores all the TMBundle class headers

---
+++ arc/arc/General/TMBundleProcessing/TMBundleHeader.h
@@ -0,0 +1,15 @@
+//
+//  TMBundleHeader.h
+//  arc
+//
+//  Created by Benedict Liang on 26/3/13.
+//  Copyright (c) 2013 nus.cs3217. All rights reserved.
+//
+
+#ifndef arc_TMBundleHeader_h
+#define arc_TMBundleHeader_h
+
+#include "TMBundleSyntaxParser.h"
+
+#endif
+
