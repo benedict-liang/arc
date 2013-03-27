7cfcc9398c550cb957d9c1b7df628e59b0c7fe21
Omer Iqbal <momeriqbal2@gmail.com>
2013-03-28 02:32:58 +0800

    added a basic Syntax Highlighting middleware

---
+++ arc/arc/View/Middleware/SyntaxHighlight.h
@@ -0,0 +1,16 @@
+//
+//  SyntaxHighlight.h
+//  arc
+//
+//  Created by omer iqbal on 28/3/13.
+//  Copyright (c) 2013 nus.cs3217. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+#import <CoreText/CoreText.h>
+#import "CodeViewMiddleware.h"
+@interface SyntaxHighlight : NSObject <CodeViewMiddleware>
+@property NSArray* patterns;
+@property NSString* content;
+@property NSMutableAttributedString* output;
+@end
---
+++ arc/arc/View/Middleware/SyntaxHighlight.m
@@ -0,0 +1,59 @@
+//
+//  SyntaxHighlight.m
+//  arc
+//
+//  Created by omer iqbal on 28/3/13.
+//  Copyright (c) 2013 nus.cs3217. All rights reserved.
+//
+
+#import "SyntaxHighlight.h"
+
+@implementation SyntaxHighlight
+-(void)initPatterns {
+    _patterns = @[
+    @{@"keyword": @{@"patterns": @[@"if", @"while", @"\\{", @"\\}", @"\\[", @"\\]", @"@property"],
+                    @"foreground": [UIColor redColor]}}
+    ];
+}
+-(NSArray*)foundPattern:(NSString*)p {
+    NSError *error = NULL;
+    NSMutableArray* results = [[NSMutableArray alloc] init];
+    NSRegularExpression *regex = [NSRegularExpression
+                                  regularExpressionWithPattern:p
+                                  options:NSRegularExpressionCaseInsensitive
+                                  error:&error];
+    NSArray* matches = [regex matchesInString:_content options:0 range:NSMakeRange(0, [_content length])];
+    
+    for (NSTextCheckingResult *match in matches) {
+        [results addObject:[NSValue value:(__bridge const void *)(match) withObjCType:@encode(NSRange)]];
+    }
+    return results;
+}
+-(void)styleOnRange:(NSRange)range fcolor:(UIColor*)fcolor {
+    [_output addAttribute:(id)kCTForegroundColorAttributeName
+                    value:fcolor range:range];
+    
+}
+-(void)iterPatternsAndApply {
+    for (NSDictionary* apattern in _patterns) {
+        for (id descriptor in apattern) {
+            NSDictionary* record = [apattern objectForKey:descriptor];
+            NSArray* regexes = [record objectForKey:@"patterns"];
+            UIColor* color = [record objectForKey:@"foreground"];
+            for (NSString* p in regexes) {
+                NSArray* regexRanges = [self foundPattern:p];
+                for (NSValue* v in regexRanges) {
+                    NSRange range;
+                    [v getValue:&range];
+                    [self styleOnRange:range fcolor:color];
+                }
+            }
+        }
+    }
+}
+-(void)execOn:(NSMutableAttributedString *)attributedString FromFile:(File *)file {
+    [self initPatterns];
+    _content = [file getContents];
+    [self iterPatternsAndApply];
+}
+@end
