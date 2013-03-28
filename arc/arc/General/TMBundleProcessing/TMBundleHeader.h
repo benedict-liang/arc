12ab56cbd990d16493035c73461c4cd49fe4dc4e
Benedict Liang <benedictljj@gmail.com>
2013-03-28 17:16:44 +0800

    Added TMBundleHeader.
    Add all TextMate processing file headers to this header file.

--- arc/arc/General/TMBundleProcessing/TMBundleHeader.h
+++ arc/arc/General/TMBundleProcessing/TMBundleHeader.h
@@ -6,10 +6,4 @@
 //  Copyright (c) 2013 nus.cs3217. All rights reserved.
 //
 
-#ifndef arc_TMBundleHeader_h
-#define arc_TMBundleHeader_h
-
-#include "TMBundleSyntaxParser.h"
-
-#endif
-
+#import "TMBundleSyntaxParser.h"
