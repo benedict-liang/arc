3f7bf64b297b071206079cbf059d0846f304b46a
Jerome Cheng <jcheng31@gmail.com>
2013-03-28 15:08:00 +0800

    Add test for creating folder.

--- arc/arcTests/arcTests.m
+++ arc/arcTests/arcTests.m
@@ -7,6 +7,7 @@
 //
 
 #import "arcTests.h"
+#import "Constants.h"
 
 @implementation arcTests
 
@@ -17,6 +18,12 @@
     
     // Set-up code here.
     _rootFolder = [RootFolder getInstance];
+    
+    for (FileObject *current in [_rootFolder getContents]) {
+        if (![[current name] isEqualToString:FOLDER_EXTERNAL_APPLICATIONS]) {
+            [current remove];
+        }
+    }
 }
 
 - (void)tearDown
@@ -24,9 +31,6 @@
     // Tear-down code here.
     
     [super tearDown];
-    for (FileObject *current in [_rootFolder getContents]) {
-        [current remove];
-    }
 }
 
 - (void)testCreateFiles
@@ -45,10 +49,41 @@
     for (FileObject *currentObject in rootContents) {
         if ([currentObject isKindOfClass:[File class]]) {
             retrieved = (File*)currentObject;
+            break;
         }
     }
-    STAssertTrue([testFileName isEqualToString:[retrieved name]], @"File name is persisted.");
-    STAssertTrue([testFileContents isEqualToString:[retrieved getContents]], @"File contents are persisted.");
+    
+    NSString *retrievedName = [retrieved name];
+    NSString *retrievedContents = [retrieved getContents];
+    
+    STAssertNotNil(retrieved, @"File is persisted and retrievable.");
+    STAssertTrue([testFileName isEqualToString:retrievedName], @"File name is persisted.");
+    STAssertTrue([testFileContents isEqualToString:retrievedContents], @"File contents are persisted.");
+}
+
+- (void)testCreateFolder
+{
+    NSString *testFolderName = @"Test Folder";
+    
+    [_rootFolder createFolder:testFolderName];
+    
+    NSArray *rootContents = [_rootFolder getContents];
+    int expected = 2;
+    int actual = [rootContents count];
+    STAssertEquals(expected, actual, @"Root folder contains 2 objects");
+    
+    Folder *retrieved;
+    for (FileObject *currentObject in rootContents) {
+        if ([currentObject isKindOfClass:[Folder class]] && [[currentObject name] isEqualToString:testFolderName]) {
+            retrieved = (Folder*)currentObject;
+            break;
+        }
+    }
+    
+    NSString *retrievedName = [retrieved name];
+    
+    STAssertNotNil(retrieved, @"Folder is persisted and retrievable.");
+    STAssertTrue([testFolderName isEqualToString:retrievedName], @"Folder name is persisted.");
 }
 
 @end
