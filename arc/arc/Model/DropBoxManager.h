753abdba3126aeef44c48a45ee886e6628ad624c
Jerome Cheng <jcheng31@gmail.com>
2013-03-29 17:12:21 +0800

    Add DropBoxManager class.

---
+++ arc/arc/Model/DropBoxManager.h
@@ -0,0 +1,13 @@
+//
+//  DropBoxManager.h
+//  arc
+//
+//  Created by Jerome Cheng on 29/3/13.
+//  Copyright (c) 2013 nus.cs3217. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface DropBoxManager : NSObject
+
+@end
---
+++ arc/arc/Model/DropBoxManager.m
@@ -0,0 +1,13 @@
+//
+//  DropBoxManager.m
+//  arc
+//
+//  Created by Jerome Cheng on 29/3/13.
+//  Copyright (c) 2013 nus.cs3217. All rights reserved.
+//
+
+#import "DropBoxManager.h"
+
+@implementation DropBoxManager
+
+@end
