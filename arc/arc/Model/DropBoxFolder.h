557d0337562441a1033403a0658ba11c8c23a8a9
Jerome Cheng <jcheng31@gmail.com>
2013-03-30 14:48:46 +0800

    Add DropBoxFolder.

---
+++ arc/arc/Model/DropBoxFolder.h
@@ -0,0 +1,13 @@
+//
+//  DropBoxFolder.h
+//  arc
+//
+//  Created by Jerome Cheng on 30/3/13.
+//  Copyright (c) 2013 nus.cs3217. All rights reserved.
+//
+
+#import "Folder.h"
+
+@interface DropBoxFolder : Folder
+
+@end
