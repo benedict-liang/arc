46f047a489ca53bbd8f04fc2687496b362b9e80d
Jerome Cheng <jcheng31@gmail.com>
2013-04-01 10:50:30 +0800

    Add LocalFolder, remove old Folder header.

---
+++ arc/arc/Model/LocalFolder.h
@@ -0,0 +1,13 @@
+//
+//  LocalFolder.h
+//  arc
+//
+//  Created by Jerome Cheng on 1/4/13.
+//  Copyright (c) 2013 nus.cs3217. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface LocalFolder : NSObject
+
+@end
---
+++ arc/arc/Model/LocalFolder.m
@@ -0,0 +1,13 @@
+//
+//  LocalFolder.m
+//  arc
+//
+//  Created by Jerome Cheng on 1/4/13.
+//  Copyright (c) 2013 nus.cs3217. All rights reserved.
+//
+
+#import "LocalFolder.h"
+
+@implementation LocalFolder
+
+@end
