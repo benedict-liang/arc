b0bf9470e1dcdb23d41680e854183e0a5b0ad806
Jerome Cheng <jcheng31@gmail.com>
2013-04-01 11:36:29 +0800

    Implement init in LocalFolder.

--- arc/arc/Model/LocalFolder.m
+++ arc/arc/Model/LocalFolder.m
@@ -10,4 +10,20 @@
 
 @implementation LocalFolder
 
+// Synthesize properties from protocol.
+@synthesize name=_name, path=_path, parent=_parent;
+
+// Initialises this object with the given name, path, and parent.
+- (id)initWithName:(NSString *)name path:(NSString *)path parent:(id<FileSystemObject>)parent
+{
+    if (self = [super init]) {
+        _name = name;
+        _path = path;
+        _parent = parent;
+        _needsRefresh = YES;
+    }
+    return self;
+}
+
+
 @end
