73efeb588b352948ba52c84194f06fcbf56484e9
Jerome Cheng <jcheng31@gmail.com>
2013-04-01 18:18:15 +0800

    Implement DropBoxFile:initWithName:path:parent:

--- arc/arc/Model/DropBoxFile.m
+++ arc/arc/Model/DropBoxFile.m
@@ -13,5 +13,18 @@
 // Synthesize properties from protocol.
 @synthesize name=_name, path=_path, parent=_parent, extension=_extension;
 
+// Initialises this object with the given name, path, and parent.
+- (id)initWithName:(NSString *)name path:(NSString *)path parent:(id<FileSystemObject>)parent
+{
+    if (self = [super init]) {
+        _name = name;
+        _path = path;
+        _parent = parent;
+        _extension = [name pathExtension];
+        _needsRefresh = YES;
+    }
+    return self;
+}
+
 
 @end
