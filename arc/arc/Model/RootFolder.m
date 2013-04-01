ec1cace7c3e893b6e3c8c1bb646bea9b49a95364
Jerome Cheng <jcheng31@gmail.com>
2013-04-01 19:18:32 +0800

    Implement RootFolder init.

--- arc/arc/Model/RootFolder.m
+++ arc/arc/Model/RootFolder.m
@@ -12,4 +12,15 @@ static RootFolder *sharedRootFolder = nil;
 
 @implementation RootFolder
 
+// Synthesize properties from protocol.
+@synthesize name=_name, path=_path, parent=_parent;
+
++ (RootFolder *)sharedRootFolder
+{
+    if (sharedRootFolder == nil) {
+        sharedRootFolder = [[super allocWithZone:NULL] init];
+    }
+    return sharedRootFolder;
+}
+
 @end
