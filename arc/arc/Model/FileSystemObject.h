118ba30d2f529238739d1575a2585f5cfc655b98
Jerome Cheng <jcheng31@gmail.com>
2013-04-01 10:13:16 +0800

    Add FileSystemObject protocol file.

---
+++ arc/arc/Model/FileSystemObject.h
@@ -0,0 +1,13 @@
+//
+//  FileSystemObject.h
+//  arc
+//
+//  Created by Jerome Cheng on 1/4/13.
+//  Copyright (c) 2013 nus.cs3217. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@protocol FileSystemObject <NSObject>
+
+@end
