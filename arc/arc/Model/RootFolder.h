6f8376d68fd929761d86e1764a421c627bdce2dc
Jerome Cheng <jcheng31@gmail.com>
2013-04-01 19:17:29 +0800

    Add RootFoldder header comment.

--- arc/arc/Model/RootFolder.h
+++ arc/arc/Model/RootFolder.h
@@ -8,8 +8,26 @@
 
 #import <Foundation/Foundation.h>
 #import "Folder.h"
+#import "DropBoxRootFolder.h"
+#import "LocalRootFolder.h"
 
-@interface RootFolder : NSObject <Folder>
+/**
+ RootFolder is a wrapper around LocalRootFolder, used to allow
+ multiple file systems to co-exist.
+ 
+ (arc) supports DropBox's Sync API, which uses its own internal
+ file system. In addition, the native iOS file system is used
+ for all other services.
+ 
+ RootFolder passes all messages received to LocalRootFolder's instance,
+ and only interferes with refreshContents: it takes the array returned
+ by LocalRootFolder, and adds the DropBoxRootFolder to it.
+ */
+
+@interface RootFolder : NSObject <Folder> {
+    NSArray *_contents;
+    BOOL _needsRefresh;
+}
 
 + (RootFolder *)sharedRootFolder;
 
