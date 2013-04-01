cca48da34a5c37d84163d122d29a7ab16db7a0d7
Jerome Cheng <jcheng31@gmail.com>
2013-04-01 18:57:53 +0800

    Add basic RootFolder outline.

---
+++ arc/arc/Model/RootFolder.h
@@ -0,0 +1,16 @@
+//
+//  RootFolder.h
+//  arc
+//
+//  Created by Jerome Cheng on 1/4/13.
+//  Copyright (c) 2013 nus.cs3217. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+#import "Folder.h"
+
+@interface RootFolder : NSObject <Folder>
+
++ (RootFolder *)sharedRootFolder;
+
+@end
---
+++ arc/arc/Model/RootFolder.m
@@ -0,0 +1,15 @@
+//
+//  RootFolder.m
+//  arc
+//
+//  Created by Jerome Cheng on 1/4/13.
+//  Copyright (c) 2013 nus.cs3217. All rights reserved.
+//
+
+#import "RootFolder.h"
+
+static RootFolder *sharedRootFolder = nil;
+
+@implementation RootFolder
+
+@end
