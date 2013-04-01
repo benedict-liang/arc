332ba490373685780e84a4b95e8d0a2d2933da04
Jerome Cheng <jcheng31@gmail.com>
2013-04-01 18:34:17 +0800

    Implement DropBoxRootFolder:init.

--- arc/arc/Model/DropBoxRootFolder.m
+++ arc/arc/Model/DropBoxRootFolder.m
@@ -8,6 +8,27 @@
 
 #import "DropBoxRootFolder.h"
 
+static DropBoxRootFolder *sharedDropBoxRootFolder = nil;
+
 @implementation DropBoxRootFolder
+
++ (DropBoxRootFolder *)sharedDropBoxRootFolder
+{
+    if (sharedDropBoxRootFolder == nil) {
+        sharedDropBoxRootFolder = [[super allocWithZone:NULL] init];
+    }
+    return sharedDropBoxRootFolder;
+}
+
+- (id)init
+{
+    DBPath *rootPath = [DBPath root];
+    NSString *ourPath = [rootPath stringValue];
+    
+    if (self = [super initWithName:FOLDER_DROPBOX_ROOT path:ourPath parent:nil]) {
+        
+    }
+    return self;
+}
 
 @end
