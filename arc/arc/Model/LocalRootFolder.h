bf410e50572679047b019d7fa574f99527277d37
Jerome Cheng <jcheng31@gmail.com>
2013-04-01 12:26:36 +0800

    Add LocalRootFolder.

---
+++ arc/arc/Model/LocalRootFolder.h
@@ -0,0 +1,13 @@
+//
+//  LocalRootFolder.h
+//  arc
+//
+//  Created by Jerome Cheng on 1/4/13.
+//  Copyright (c) 2013 nus.cs3217. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface LocalRootFolder : NSObject
+
+@end
---
+++ arc/arc/Model/LocalRootFolder.m
@@ -0,0 +1,13 @@
+//
+//  LocalRootFolder.m
+//  arc
+//
+//  Created by Jerome Cheng on 1/4/13.
+//  Copyright (c) 2013 nus.cs3217. All rights reserved.
+//
+
+#import "LocalRootFolder.h"
+
+@implementation LocalRootFolder
+
+@end
