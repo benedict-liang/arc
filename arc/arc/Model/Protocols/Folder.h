b7ab583ae01e34d361577c178ba808b57fac942a
Jerome Cheng <jcheng31@gmail.com>
2013-04-01 10:29:55 +0800

    Add Folder protocol.

---
+++ arc/arc/Model/Protocols/Folder.h
@@ -0,0 +1,13 @@
+//
+//  Folder.h
+//  arc
+//
+//  Created by Jerome Cheng on 1/4/13.
+//  Copyright (c) 2013 nus.cs3217. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@protocol Folder <NSObject>
+
+@end
