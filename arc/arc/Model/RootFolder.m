5d671233f0c6dd6a9774b2666724a41074deb7e1
Jerome Cheng <jcheng31@gmail.com>
2013-04-01 19:19:18 +0800

    Implement remaining RootFolder methods.

--- arc/arc/Model/RootFolder.m
+++ arc/arc/Model/RootFolder.m
@@ -43,4 +43,55 @@ static RootFolder *sharedRootFolder = nil;
     return _contents;
 }
 
+// Marks this object as needing to be refreshed.
+- (void)markNeedsRefresh
+{
+    [[LocalRootFolder sharedLocalRootFolder] markNeedsRefresh];
+}
+
+// Initialises this object with the given name, path, and parent.
+- (id)initWithName:(NSString *)name path:(NSString *)path parent:(id<FileSystemObject>)parent
+{
+    // RootFolder can't be initialised manually.
+    @throw [NSException exceptionWithName:NSInternalInconsistencyException reason:[NSString stringWithFormat:@"RootFolder doesn't allow %@", NSStringFromSelector(_cmd)] userInfo:nil];
+}
+
+// Moves the given FileSystemObject to this Folder.
+// The given file must be of the same "type" as this Folder
+// (e.g. iOS file system, DropBox, etc.)
+// Returns YES if successful, NO otherwise.
+- (BOOL)takeFileSystemObject:(id<FileSystemObject>)target
+{
+    return [[LocalRootFolder sharedLocalRootFolder] takeFileSystemObject:target];
+}
+
+// Returns the FileSystemObject with the given name.
+// Will return nil if the object is not found.
+- (id<FileSystemObject>)retrieveItemWithName:(NSString *)name
+{
+    return [[LocalRootFolder sharedLocalRootFolder] retrieveItemWithName:name];
+}
+
+// Creates a Folder with the given name inside this one.
+// Returns the created Folder object.
+- (id<Folder>)createFolderWithName:(NSString *)name
+{
+    return [[LocalRootFolder sharedLocalRootFolder] createFolderWithName:name];
+}
+
+
+// Renames this Folder to the given name.
+- (BOOL)rename:(NSString *)name
+{
+    return [[LocalRootFolder sharedLocalRootFolder] rename:name];
+}
+
+// Removes this object.
+// Returns YES if successful, NO otherwise.
+// If NO is returned, the state of the object or its contents is unstable.
+- (BOOL)remove
+{
+    return [[LocalRootFolder sharedLocalRootFolder] remove];
+}
+
 @end
