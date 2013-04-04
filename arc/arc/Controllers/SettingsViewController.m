488086c529d6e2e094df63775a9c2fec8fb4bc4f
Jerome Cheng <jcheng31@gmail.com>
2013-04-04 23:25:38 +0800

    Enable settings to be changed and saved.

--- arc/arc/Controllers/SettingsViewController.m
+++ arc/arc/Controllers/SettingsViewController.m
@@ -114,7 +114,24 @@
 
 - (void)tableView:(UITableView*)tableView didSelectRowAtIndexPath:(NSIndexPath*)indexPath
 {
-
+    // Get the properties associated with this section.
+    NSDictionary *sectionProperties = [_options objectAtIndex:indexPath.section];
+    NSDictionary *options = [sectionProperties objectForKey:@"keyValuePairs"];
+    NSArray *allKeys = [options allKeys];
+    NSArray *allValues = [options objectsForKeys:allKeys notFoundMarker:@"None"];
+    
+    // Get the value associated with the row.
+    NSString *value = [allValues objectAtIndex:indexPath.row];
+    
+    ApplicationState *state = [ApplicationState sharedApplicationState];
+    
+    // Update the appropriate setting using the key associated with this section.
+    NSString *currentKey = [sectionProperties valueForKey:@"settingsKey"];
+    [state setSetting:value forKey:currentKey];
+    [state saveStateToDisk];
+    
+    // Add a checkmark to the row.
+    [[tableView cellForRowAtIndexPath:indexPath] setAccessoryType:UITableViewCellAccessoryCheckmark];
 }
 
 @end
