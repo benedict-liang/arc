6236de56eaddcfb22334ac22031430d3e68ee06e
Omer Iqbal <momeriqbal2@gmail.com>
2013-04-06 00:08:33 +0800

    working py

--- arc/arc/View/Middleware/SyntaxHighlight.m
+++ arc/arc/View/Middleware/SyntaxHighlight.m
@@ -22,7 +22,7 @@
     if (self) {
         _delegate = d;
         _currentFile = file;
-        _patterns = [TMBundleSyntaxParser getPatternsArray:@"source.js.plist"];
+        _patterns = [TMBundleSyntaxParser getPatternsArray:@"source.python.plist"];
         _theme = [TMBundleThemeHandler produceStylesWithTheme:nil];
         if ([[file contents] isKindOfClass:[NSString class]]) {
             _content = (NSString*)[file contents];
