8ee9c365078d48dccbd0aeb7dea26d053a911735
Michael Yong <wrong92@gmail.com>
2013-04-06 17:04:50 +0800

    More Plugin skeletons

---
+++ arc/arc/Plugin/FontFamilyPlugin.m
@@ -0,0 +1,13 @@
+//
+//  FontFamilyPlugin.m
+//  arc
+//
+//  Created by Yong Michael on 6/4/13.
+//  Copyright (c) 2013 nus.cs3217. All rights reserved.
+//
+
+#import "FontFamilyPlugin.h"
+
+@implementation FontFamilyPlugin
+
+@end
---
+++ arc/arc/Plugin/FontSizePlugin.h
@@ -0,0 +1,14 @@
+//
+//  FontSizePlugin.h
+//  arc
+//
+//  Created by Yong Michael on 6/4/13.
+//  Copyright (c) 2013 nus.cs3217. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+#import "PluginDelegate.h"
+
+@interface FontSizePlugin : NSObject<PluginDelegate>
+
+@end
