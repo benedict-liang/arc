6f4221430d75fea140b04f88530b7dc1e239309c
Michael Yong <wrong92@gmail.com>
2013-04-06 20:28:31 +0800

    Added Settings View Delegate

---
+++ arc/arc/Controllers/Protocols/SettingsViewDelegate.h
@@ -0,0 +1,13 @@
+//
+//  SettingsViewDelegate.h
+//  arc
+//
+//  Created by Yong Michael on 6/4/13.
+//  Copyright (c) 2013 nus.cs3217. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@protocol SettingsViewDelegate <NSObject>
+
+@end
