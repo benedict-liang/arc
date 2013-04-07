ea58f04c2c07a0226b6177502ff09fc3d75eabb9
Omer Iqbal <momeriqbal2@gmail.com>
2013-04-07 22:27:04 +0800

    divided up SyntaxHighlight for async

---
+++ arc/arc/Plugin/SyntaxHighlight.h
@@ -0,0 +1,33 @@
+//
+//  SyntaxHighlight.h
+//  arc
+//
+//  Created by omer iqbal on 7/4/13.
+//  Copyright (c) 2013 nus.cs3217. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+#import "ArcAttributedString.h"
+#import "TMBundleHeader.h"
+#import "CodeViewControllerDelegate.h"
+#import "File.h"
+
+
+@interface SyntaxHighlight : NSObject {
+    __block NSDictionary *nameMatches;
+    __block NSDictionary *captureMatches;
+    __block NSDictionary *beginCMatches;
+    __block NSDictionary *endCMatches;
+    __block NSDictionary *pairMatches;
+    __block NSDictionary *contentNameMatches;
+    __block NSDictionary *overlapMatches;
+}
+@property (readonly) NSDictionary* theme;
+@property (readonly) id<CodeViewControllerDelegate> delegate;
+@property (readonly) id<File> currentFile;
+@property (readonly) NSString* content;
+@property (readonly) NSDictionary* bundle;
+@property NSArray* overlays;
+
+- (id)initWithFile:(id<File>)file del:(id<CodeViewControllerDelegate>)delegate theme:(NSDictionary*)theme;
+@end
--- arc/arc/Plugin/SyntaxHighlightingPlugin.h
+++ arc/arc/Plugin/SyntaxHighlightingPlugin.h
@@ -12,21 +12,12 @@
 #import "TMBundleHeader.h"
 #import "CodeViewControllerDelegate.h"
 #import "File.h"
+#import "SyntaxHighlight.h"
 
 @interface SyntaxHighlightingPlugin : NSObject<PluginDelegate> {
-    __block NSDictionary *nameMatches;
-    __block NSDictionary *captureMatches;
-    __block NSDictionary *beginCMatches;
-    __block NSDictionary *endCMatches;
-    __block NSDictionary *pairMatches;
-    __block NSDictionary *contentNameMatches;
-    __block NSDictionary *overlapMatches;
+ 
 }
-@property NSDictionary* theme;
-@property (readonly) id<CodeViewControllerDelegate> delegate;
-@property (readonly) id<File> currentFile;
-@property (readonly) NSString* content;
-@property (readonly) NSDictionary* bundle;
-@property NSArray* overlays;
 
+@property (readonly) id<CodeViewControllerDelegate> delegate;
+@property NSDictionary* theme;
 @end
