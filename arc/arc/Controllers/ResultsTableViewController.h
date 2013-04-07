b5c20182116cd615335bf0b3fa8a02e710810804
Benedict Liang <benedictljj@gmail.com>
2013-04-07 22:45:41 +0800

    Added View Controller for Results Popover View

---
+++ arc/arc/Controllers/ResultsTableViewController.h
@@ -0,0 +1,13 @@
+//
+//  ResultsTableViewController.h
+//  arc
+//
+//  Created by Benedict Liang on 7/4/13.
+//  Copyright (c) 2013 nus.cs3217. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ResultsTableViewController : UITableViewController
+
+@end
