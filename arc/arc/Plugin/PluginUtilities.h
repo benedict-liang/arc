6a3b07a2db0deac3431902899d13cc297ca29a46
Jerome Cheng <jcheng31@gmail.com>
2013-04-07 16:44:33 +0800

    Create PluginUtilities class.

---
+++ arc/arc/Plugin/PluginUtilities.h
@@ -0,0 +1,13 @@
+//
+//  PluginUtilities.h
+//  arc
+//
+//  Created by Jerome Cheng on 7/4/13.
+//  Copyright (c) 2013 nus.cs3217. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface PluginUtilities : NSObject
+
+@end
---
+++ arc/arc/Plugin/PluginUtilities.m
@@ -0,0 +1,13 @@
+//
+//  PluginUtilities.m
+//  arc
+//
+//  Created by Jerome Cheng on 7/4/13.
+//  Copyright (c) 2013 nus.cs3217. All rights reserved.
+//
+
+#import "PluginUtilities.h"
+
+@implementation PluginUtilities
+
+@end
