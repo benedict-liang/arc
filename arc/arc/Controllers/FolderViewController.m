fea18650932d60c1f1d98d98f7007f28af89c65b
Jerome Cheng <jcheng31@gmail.com>
2013-04-08 17:18:51 +0800

    Add ability to delete files/folders.

--- arc/arc/Controllers/FolderViewController.m
+++ arc/arc/Controllers/FolderViewController.m
@@ -161,5 +161,18 @@ titleForHeaderInSection:(NSInteger)section {
     [_tableView setEditing:shouldTableEdit animated:YES];
 }
 
+- (void)tableView:(UITableView *)tableView commitEditingStyle:(UITableViewCellEditingStyle)editingStyle forRowAtIndexPath:(NSIndexPath *)indexPath
+{
+    if (editingStyle == UITableViewCellEditingStyleDelete) {
+        NSArray *currentSection = [_filesAndFolders objectAtIndex:indexPath.section];
+        id<FileSystemObject> fileObject = [currentSection objectAtIndex:indexPath.row];
+        
+        // TODO: check if object is allowed to be deleted. This is probably in a data-source delegate.
+        if ([fileObject remove]) {
+            [(NSMutableArray *)[_filesAndFolders objectAtIndex:indexPath.section] removeObjectAtIndex:indexPath.row];
+            [tableView deleteRowsAtIndexPaths:[NSArray arrayWithObject:indexPath] withRowAnimation:UITableViewRowAnimationAutomatic];
+        }
+    }
+}
 
 @end
