4397b2034c2e6147c951c0f9bceb7fc7d4d18f9b
Jerome Cheng <jcheng31@gmail.com>
2013-04-09 12:24:19 +0800

    Create group for popover controllers.

---
+++ arc/arc/Controllers/CreateFolderViewController.h
@@ -0,0 +1,13 @@
+//
+//  CreateFolderViewController.h
+//  arc
+//
+//  Created by Jerome Cheng on 9/4/13.
+//  Copyright (c) 2013 nus.cs3217. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface CreateFolderViewController : UIViewController
+
+@end
