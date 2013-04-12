6f16c6a80b3b7570d3156d4c9ca81fa65aaac1e6
Jerome Cheng <jcheng31@gmail.com>
2013-04-12 16:59:49 +0800

    Add new delegate for long list.

---
+++ arc/arc/Controllers/Protocols/LongSettingListViewControllerDelegate.h
@@ -0,0 +1,13 @@
+//
+//  LongSettingListViewControllerDelegate.h
+//  arc
+//
+//  Created by Jerome Cheng on 12/4/13.
+//  Copyright (c) 2013 nus.cs3217. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@protocol LongSettingListViewControllerDelegate <NSObject>
+
+@end
