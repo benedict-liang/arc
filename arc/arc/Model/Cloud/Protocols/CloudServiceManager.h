bca47e5e25b5ddd87329a01fe9a7834b0aef22a3
Jerome Cheng <jcheng31@gmail.com>
2013-04-13 19:09:31 +0800

    Add preliminary CloudServiceManager method headers.

--- arc/arc/Model/Cloud/Protocols/CloudServiceManager.h
+++ arc/arc/Model/Cloud/Protocols/CloudServiceManager.h
@@ -1,11 +1,20 @@
 //
 // Created by Jerome on 13/4/13.
 //
-// To change the template use AppCode | Preferences | File Templates.
 //
 
-
 #import <Foundation/Foundation.h>
+#import "LocalFolder.h"
 
 @protocol CloudServiceManager <NSObject>
+
+// Takes in a ViewController and an optional delegate.
+// Triggers the cloud service's login procedure.
++ (void)loginWithViewController:(UIViewController *)controller delegate:(id)delegate;
+
+// Takes in a path on the file service, and a LocalFolder.
+// Downloads the file at that path and stores it in the LocalFolder.
+// Returns YES if successful, NO otherwise.
++ (BOOL)downloadFileAtPath:(NSString *)path toFolder:(LocalFolder *)folder;
+
 @end
