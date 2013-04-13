660248fcd5ada376eb93104446ba65b9a97af394
Jerome Cheng <jcheng31@gmail.com>
2013-04-13 19:20:06 +0800

    Add SkyDriveServiceManager files.

---
+++ arc/arc/Model/Cloud/SkyDriveServiceManager.h
@@ -0,0 +1,12 @@
+//
+// Created by Jerome on 13/4/13.
+//
+// To change the template use AppCode | Preferences | File Templates.
+//
+
+
+#import <Foundation/Foundation.h>
+
+
+@interface SkyDriveServiceManager : NSObject
+@end
---
+++ arc/arc/Model/Cloud/SkyDriveServiceManager.m
@@ -0,0 +1,14 @@
+//
+// Created by Jerome on 13/4/13.
+//
+// To change the template use AppCode | Preferences | File Templates.
+//
+
+
+#import "SkyDriveServiceManager.h"
+
+
+@implementation SkyDriveServiceManager {
+
+}
+@end
