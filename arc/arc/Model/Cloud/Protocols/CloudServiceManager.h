9daa1ee85f9e4d46d4500922e888caa80d91652e
Jerome Cheng <jcheng31@gmail.com>
2013-04-13 18:00:18 +0800

    Add CloudServiceManager file.

---
+++ arc/arc/Model/Cloud/Protocols/CloudServiceManager.h
@@ -0,0 +1,11 @@
+//
+// Created by Jerome on 13/4/13.
+//
+// To change the template use AppCode | Preferences | File Templates.
+//
+
+
+#import <Foundation/Foundation.h>
+
+@protocol CloudServiceManager <NSObject>
+@end
