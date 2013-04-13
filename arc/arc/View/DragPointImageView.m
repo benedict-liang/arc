1a2212f13a4089f2203bff1ba26ceb58ab8ba76c
Benedict Liang <benedictljj@gmail.com>
2013-04-14 02:18:49 +0800

    Added dragpointimageview.

---
+++ arc/arc/View/DragPointImageView.m
@@ -0,0 +1,31 @@
+//
+//  DragPointImageView.m
+//  arc
+//
+//  Created by Benedict Liang on 14/4/13.
+//  Copyright (c) 2013 nus.cs3217. All rights reserved.
+//
+
+#import "DragPointImageView.h"
+
+@implementation DragPointImageView
+
+- (id)initWithFrame:(CGRect)frame
+{
+    self = [super initWithFrame:frame];
+    if (self) {
+        // Initialization code
+    }
+    return self;
+}
+
+/*
+// Only override drawRect: if you perform custom drawing.
+// An empty implementation adversely affects performance during animation.
+- (void)drawRect:(CGRect)rect
+{
+    // Drawing code
+}
+*/
+
+@end
