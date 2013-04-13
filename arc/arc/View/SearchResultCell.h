fd45c4973bd661b0aa452d52ca061b9947ef74b2
Benedict Liang <benedictljj@gmail.com>
2013-04-13 21:55:25 +0800

    Added new cell view (SearchResultCell).

---
+++ arc/arc/View/SearchResultCell.h
@@ -0,0 +1,13 @@
+//
+//  SearchResultCell.h
+//  arc
+//
+//  Created by Benedict Liang on 13/4/13.
+//  Copyright (c) 2013 nus.cs3217. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface SearchResultCell : UITableViewCell
+
+@end
