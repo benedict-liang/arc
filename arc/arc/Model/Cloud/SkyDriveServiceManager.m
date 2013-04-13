20237a6c7b75175b59f737e76a41f174c8f3e580
Jerome Cheng <jcheng31@gmail.com>
2013-04-13 19:32:01 +0800

    Add methods to init SkyDriveServiceManager.

--- arc/arc/Model/Cloud/SkyDriveServiceManager.m
+++ arc/arc/Model/Cloud/SkyDriveServiceManager.m
@@ -7,8 +7,31 @@
 
 #import "SkyDriveServiceManager.h"
 
+@interface SkyDriveServiceManager ()
+@property (strong, nonatomic) LiveConnectClient *liveClient;
+@end
 
-@implementation SkyDriveServiceManager {
 
+static SkyDriveServiceManager *sharedServiceManager = nil;
+
+@implementation SkyDriveServiceManager
+
+// Returns the singleton service manager for this particular service.
++ (id<CloudServiceManager>)sharedServiceManager
+{
+    if (sharedServiceManager == nil) {
+        sharedServiceManager = [[super allocWithZone:NULL] init];
+    }
+    return sharedServiceManager;
 }
+
+- (id)init
+{
+    if (self = [super init]) {
+        NSArray *scopesRequired = [NSArray arrayWithObjects:SKYDRIVE_SCOPE_SIGNIN, SKYDRIVE_SCOPE_READ_ACCESS];
+        _liveClient = [[LiveConnectClient alloc] initWithClientId:CLOUD_SKYDRIVE_KEY scopes:scopesRequired delegate:self]
+    }
+    return self;
+}
+
 @end
