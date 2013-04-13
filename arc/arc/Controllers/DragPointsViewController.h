bb5ff72f2f8be0be757c550ce4b4396fd615913b
Benedict Liang <benedictljj@gmail.com>
2013-04-14 02:31:20 +0800

    Added DragPointsViewController.

---
+++ arc/arc/Controllers/DragPointsViewController.h
@@ -0,0 +1,13 @@
+//
+//  DragPointsViewController.h
+//  arc
+//
+//  Created by Benedict Liang on 14/4/13.
+//  Copyright (c) 2013 nus.cs3217. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface DragPointsViewController : UIViewController
+
+@end
---
+++ arc/arc/Controllers/DragPointsViewController.m
@@ -0,0 +1,29 @@
+//
+//  DragPointsViewController.m
+//  arc
+//
+//  Created by Benedict Liang on 14/4/13.
+//  Copyright (c) 2013 nus.cs3217. All rights reserved.
+//
+
+#import "DragPointsViewController.h"
+
+@interface DragPointsViewController ()
+
+@end
+
+@implementation DragPointsViewController
+
+- (void)viewDidLoad
+{
+    [super viewDidLoad];
+	// Do any additional setup after loading the view.
+}
+
+- (void)didReceiveMemoryWarning
+{
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
