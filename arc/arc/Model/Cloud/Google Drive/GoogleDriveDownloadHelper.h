d2e39651894c2bdcb908a45924ad2a1c0cffcdac
Jerome Cheng <jcheng31@gmail.com>
2013-04-14 19:00:02 +0800

    Add GoogleDriveDownloadHelper files.

---
+++ arc/arc/Model/Cloud/Google Drive/GoogleDriveDownloadHelper.h
@@ -0,0 +1,13 @@
+//
+//  GoogleDriveDownloadHelper.h
+//  arc
+//
+//  Created by Jerome Cheng on 14/4/13.
+//  Copyright (c) 2013 nus.cs3217. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface GoogleDriveDownloadHelper : NSObject
+
+@end
