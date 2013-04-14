46399f3d5a2ba21ec730f1355a080088915b1665
Jerome Cheng <jcheng31@gmail.com>
2013-04-14 17:15:31 +0800

    Add GoogleDriveFolder files.

---
+++ arc/arc/Model/Cloud/Google Drive/GoogleDriveFolder.h
@@ -0,0 +1,13 @@
+//
+//  GoogleDriveFolder.h
+//  arc
+//
+//  Created by Jerome Cheng on 14/4/13.
+//  Copyright (c) 2013 nus.cs3217. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface GoogleDriveFolder : NSObject
+
+@end
---
+++ arc/arc/Model/Cloud/Google Drive/GoogleDriveFolder.m
@@ -0,0 +1,13 @@
+//
+//  GoogleDriveFolder.m
+//  arc
+//
+//  Created by Jerome Cheng on 14/4/13.
+//  Copyright (c) 2013 nus.cs3217. All rights reserved.
+//
+
+#import "GoogleDriveFolder.h"
+
+@implementation GoogleDriveFolder
+
+@end
