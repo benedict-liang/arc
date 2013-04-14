2b4ea636b0bce3f4e10dedfd5a946753dd6ad159
Jerome Cheng <jcheng31@gmail.com>
2013-04-14 16:11:15 +0800

    Add GoogleDriveServiceManager file.

---
+++ arc/arc/Model/Cloud/Google Drive/GoogleDriveServiceManager.m
@@ -0,0 +1,13 @@
+//
+//  GoogleDriveServiceManager.m
+//  arc
+//
+//  Created by Jerome Cheng on 14/4/13.
+//  Copyright (c) 2013 nus.cs3217. All rights reserved.
+//
+
+#import "GoogleDriveServiceManager.h"
+
+@implementation GoogleDriveServiceManager
+
+@end
