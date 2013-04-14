02342f55c3c5d3eecca3a15c2e96884d2a51c4d4
Jerome Cheng <jcheng31@gmail.com>
2013-04-14 12:32:32 +0800

    Add SkyDriveFile.

---
+++ arc/arc/Model/Cloud/SkyDrive/SkyDriveFile.m
@@ -0,0 +1,13 @@
+//
+//  SkyDriveFile.m
+//  arc
+//
+//  Created by Jerome Cheng on 14/4/13.
+//  Copyright (c) 2013 nus.cs3217. All rights reserved.
+//
+
+#import "SkyDriveFile.h"
+
+@implementation SkyDriveFile
+
+@end
