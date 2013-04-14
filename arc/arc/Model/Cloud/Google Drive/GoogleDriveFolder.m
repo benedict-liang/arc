78de5417f72540f0a9de7c35e24a06f16e192adf
Jerome Cheng <jcheng31@gmail.com>
2013-04-14 18:19:30 +0800

    Implement Google Drive folder content retrieval.

--- arc/arc/Model/Cloud/Google Drive/GoogleDriveFolder.m
+++ arc/arc/Model/Cloud/Google Drive/GoogleDriveFolder.m
@@ -76,5 +76,54 @@
     return self;
 }
 
+- (void)updateContents
+{
+    GoogleDriveServiceManager *serviceManager = (GoogleDriveServiceManager *)[GoogleDriveServiceManager sharedServiceManager];
+    GTLServiceDrive *driveService = [serviceManager driveService];
+    
+    GTLQuery *folderContentsQuery = [GTLQueryDrive queryForChildrenListWithFolderId:_path];
+    
+    [driveService executeQuery:folderContentsQuery delegate:self didFinishSelector:@selector(contentsTicket:children:error:)];
+}
+
+// Handles callbacks from a query for a folder's children.
+- (void)contentsTicket:(GTLServiceTicket *)ticket children:(GTLDriveChildList *)children error:(NSError *)error
+{
+    if (!error) {
+        GoogleDriveServiceManager *serviceManager = (GoogleDriveServiceManager *)[GoogleDriveServiceManager sharedServiceManager];
+        GTLServiceDrive *driveService = [serviceManager driveService];
+        
+        for (GTLDriveChildReference *currentReference in children) {
+            // Get the child's attributes.
+            GTLQuery *attributeQuery = [GTLQueryDrive queryForFilesGetWithFileId:[currentReference identifier]];
+            [driveService executeQuery:attributeQuery delegate:self didFinishSelector:@selector(attributesTicket:file:error:)];
+        }
+    } else {
+        NSLog(@"%@", error);
+    }
+}
+
+// Handles callbacks from a query for a file/folder's attributes.
+- (void)attributesTicket:(GTLServiceTicket *)ticket file:(GTLDriveFile *)file error:(NSError *)error
+{
+    if (!error) {
+        NSString *fileName = [file title];
+        NSString *filePath = [file downloadUrl];
+        
+        if ([[fileName pathExtension] isEqualToString:@""]) {
+            // No extension means this is a folder.
+            // Note that folders are retrieved by their identifier, not the download URL.
+            GoogleDriveFolder *newFolder = [[GoogleDriveFolder alloc] initWithName:fileName path:[file identifier] parent:self];
+            [_contents addObject:newFolder];
+        } else {
+            // There is a file extension. This must be a file.
+            GoogleDriveFile *newFile = [[GoogleDriveFile alloc] initWithName:fileName path:filePath parent:self];
+            [_contents addObject:newFile];
+        }
+        [_delegate folderContentsUpdated:self];
+    } else {
+        NSLog(@"%@", error);
+    }
+}
 
 @end
