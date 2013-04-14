36bd365db900e31c5b75ca6bef9e2ec32248bbfd
Jerome Cheng <jcheng31@gmail.com>
2013-04-14 10:52:47 +0800

    Create SkyDriveFolder files.

---
+++ arc/arc/Model/Cloud/SkyDrive/SkyDriveFolder.h
@@ -0,0 +1,12 @@
+//
+// Created by Jerome on 14/4/13.
+//
+// To change the template use AppCode | Preferences | File Templates.
+//
+
+
+#import <Foundation/Foundation.h>
+
+
+@interface SkyDriveFolder : NSObject
+@end
---
+++ arc/arc/Model/Cloud/SkyDrive/SkyDriveFolder.m
@@ -0,0 +1,14 @@
+//
+// Created by Jerome on 14/4/13.
+//
+// To change the template use AppCode | Preferences | File Templates.
+//
+
+
+#import "SkyDriveFolder.h"
+
+
+@implementation SkyDriveFolder {
+
+}
+@end
