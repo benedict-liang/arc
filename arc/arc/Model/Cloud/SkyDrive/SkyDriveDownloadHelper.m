37390d2ed3d91dfcb2c1cbb0ecaa3ca037abf8c5
Jerome Cheng <jcheng31@gmail.com>
2013-04-14 13:00:25 +0800

    Add SkyDriveDownloadHelper.

---
+++ arc/arc/Model/Cloud/SkyDrive/SkyDriveDownloadHelper.m
@@ -0,0 +1,13 @@
+//
+//  SkyDriveDownloadHelper.m
+//  arc
+//
+//  Created by Jerome Cheng on 14/4/13.
+//  Copyright (c) 2013 nus.cs3217. All rights reserved.
+//
+
+#import "SkyDriveDownloadHelper.h"
+
+@implementation SkyDriveDownloadHelper
+
+@end
