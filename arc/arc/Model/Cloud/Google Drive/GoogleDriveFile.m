1695511815c366fedc46229e4c8cb044828843cd
Jerome Cheng <jcheng31@gmail.com>
2013-04-14 18:22:43 +0800

    Add GoogleDriveFile file.

---
+++ arc/arc/Model/Cloud/Google Drive/GoogleDriveFile.m
@@ -0,0 +1,13 @@
+//
+//  GoogleDriveFile.m
+//  arc
+//
+//  Created by Jerome Cheng on 14/4/13.
+//  Copyright (c) 2013 nus.cs3217. All rights reserved.
+//
+
+#import "GoogleDriveFile.h"
+
+@implementation GoogleDriveFile
+
+@end
