40ae00e445115ad24186b77343e0047c5fb4cc78
Jerome Cheng <jcheng31@gmail.com>
2013-04-15 14:56:49 +0800

    Add CloudFile protocol.

---
+++ arc/arc/Model/Cloud/Protocols/CloudFile.h
@@ -0,0 +1,13 @@
+//
+//  CloudFile.h
+//  arc
+//
+//  Created by Jerome Cheng on 15/4/13.
+//  Copyright (c) 2013 nus.cs3217. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@protocol CloudFile <NSObject>
+
+@end
