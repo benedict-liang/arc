ce12e3cc5005cbe610af4c6053805611361cc5fc
Jerome Cheng <jcheng31@gmail.com>
2013-04-15 12:56:42 +0800

    Add CloudFolder and CloudPickerViewController files.

---
+++ arc/arc/Controllers/CloudPickerViewController.h
@@ -0,0 +1,13 @@
+//
+//  CloudPickerViewController.h
+//  arc
+//
+//  Created by Jerome Cheng on 15/4/13.
+//  Copyright (c) 2013 nus.cs3217. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface CloudPickerViewController : UITableViewController
+
+@end
---
+++ arc/arc/Model/Cloud/Protocols/CloudFolder.h
@@ -0,0 +1,13 @@
+//
+//  CloudFolder.h
+//  arc
+//
+//  Created by Jerome Cheng on 15/4/13.
+//  Copyright (c) 2013 nus.cs3217. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@protocol CloudFolder <NSObject>
+
+@end
