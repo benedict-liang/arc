5ad892eaaa288d2347b5acb624cb0e6d5c01eefd
Jerome Cheng <jcheng31@gmail.com>
2013-04-16 13:36:01 +0800

    Add CloudPickerViewControllerDelegate.

---
+++ arc/arc/Controllers/Protocols/CloudPickerViewControllerDelegate.h
@@ -0,0 +1,13 @@
+//
+//  CloudPickerViewControllerDelegate.h
+//  arc
+//
+//  Created by Jerome Cheng on 16/4/13.
+//  Copyright (c) 2013 nus.cs3217. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@protocol CloudPickerViewControllerDelegate <NSObject>
+
+@end
