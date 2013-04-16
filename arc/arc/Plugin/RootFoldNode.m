7e22542c3c27d14827a1b1473620a33149d22039
Omer Iqbal <momeriqbal2@gmail.com>
2013-04-17 02:53:52 +0800

    RootFoldNode. sigh. wish I could use structs, but oh ARC...

---
+++ arc/arc/Plugin/RootFoldNode.m
@@ -0,0 +1,16 @@
+//
+//  RootFoldNode.m
+//  arc
+//
+//  Created by omer iqbal on 17/4/13.
+//  Copyright (c) 2013 nus.cs3217. All rights reserved.
+//
+
+#import "RootFoldNode.h"
+
+@implementation RootFoldNode
+
+- (id)initWithContentRange:(NSRange)cr {
+    return [super initWithContentRange:cr startRange:NSMakeRange(0, 0) endRange:NSMakeRange(0, 0)];
+}
+@end
