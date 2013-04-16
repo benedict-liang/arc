70b55414543dc949f6868b3086e63cf9c13aee38
Jerome Cheng <jcheng31@gmail.com>
2013-04-16 21:15:56 +0800

    Add CloudServiceManagerDelegate.h

---
+++ arc/arc/Model/Cloud/CloudServiceManagerDelegate.h
@@ -0,0 +1,15 @@
+//
+//  CloudServiceManagerDelegate.h
+//  arc
+//
+//  Created by Jerome Cheng on 16/4/13.
+//  Copyright (c) 2013 nus.cs3217. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@protocol CloudServiceManagerDelegate <NSObject>
+
+- (void)downloadCompleteFromService:(id)sender;
+
+@end
