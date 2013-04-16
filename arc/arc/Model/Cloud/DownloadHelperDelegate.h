e3af89e6853cd48d86a1894c38432c8542b52cf3
Jerome Cheng <jcheng31@gmail.com>
2013-04-16 20:52:53 +0800

    Add DownloadHelperDelegate.

---
+++ arc/arc/Model/Cloud/DownloadHelperDelegate.h
@@ -0,0 +1,15 @@
+//
+//  DownloadHelperDelegate.h
+//  arc
+//
+//  Created by Jerome Cheng on 16/4/13.
+//  Copyright (c) 2013 nus.cs3217. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@protocol DownloadHelperDelegate <NSObject>
+
+- (void)downloadCompleteForHelper:(id)sender;
+
+@end
