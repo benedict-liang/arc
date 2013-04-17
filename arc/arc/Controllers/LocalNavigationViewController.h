48479b8ab0043898face037998853f3324982736
Jerome Cheng <jcheng31@gmail.com>
2013-04-17 16:02:32 +0800

    Add LocalNavigationViewController.

---
+++ arc/arc/Controllers/LocalNavigationViewController.h
@@ -0,0 +1,15 @@
+//
+//  LocalNavigationViewController.h
+//  arc
+//
+//  Created by Jerome Cheng on 17/4/13.
+//  Copyright (c) 2013 nus.cs3217. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface LocalNavigationViewController : UIViewController
+
+
+
+@end
