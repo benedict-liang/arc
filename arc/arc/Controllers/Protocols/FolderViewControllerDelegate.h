a8fe1b9adc556fb6f647b81642adff4a9c2b12a1
Jerome Cheng <jcheng31@gmail.com>
2013-04-17 16:43:01 +0800

    Add new FolderViewControllerDelegate files.

---
+++ arc/arc/Controllers/Protocols/FolderViewControllerDelegate.h
@@ -0,0 +1,13 @@
+//
+//  FolderViewControllerDelegate.h
+//  arc
+//
+//  Created by Jerome Cheng on 17/4/13.
+//  Copyright (c) 2013 nus.cs3217. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@protocol FolderViewControllerDelegate <NSObject>
+
+@end
