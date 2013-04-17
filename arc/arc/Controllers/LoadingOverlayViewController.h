eb1adbfeed996261ac864c8640ef75bcd50cea1c
Jerome Cheng <jcheng31@gmail.com>
2013-04-17 10:21:36 +0800

    Add LoadingOverlayViewController files.

---
+++ arc/arc/Controllers/LoadingOverlayViewController.h
@@ -0,0 +1,13 @@
+//
+//  LoadingOverlayViewController.h
+//  arc
+//
+//  Created by Jerome Cheng on 17/4/13.
+//  Copyright (c) 2013 nus.cs3217. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface LoadingOverlayViewController : UIViewController
+
+@end
---
+++ arc/arc/Controllers/LoadingOverlayViewController.m
@@ -0,0 +1,38 @@
+//
+//  LoadingOverlayViewController.m
+//  arc
+//
+//  Created by Jerome Cheng on 17/4/13.
+//  Copyright (c) 2013 nus.cs3217. All rights reserved.
+//
+
+#import "LoadingOverlayViewController.h"
+
+@interface LoadingOverlayViewController ()
+
+@end
+
+@implementation LoadingOverlayViewController
+
+- (id)initWithNibName:(NSString *)nibNameOrNil bundle:(NSBundle *)nibBundleOrNil
+{
+    self = [super initWithNibName:nibNameOrNil bundle:nibBundleOrNil];
+    if (self) {
+        // Custom initialization
+    }
+    return self;
+}
+
+- (void)viewDidLoad
+{
+    [super viewDidLoad];
+	// Do any additional setup after loading the view.
+}
+
+- (void)didReceiveMemoryWarning
+{
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
