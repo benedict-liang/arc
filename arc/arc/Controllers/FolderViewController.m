8eab07f337f452c252c639e9e7259f0c970710c1
Jerome Cheng <jcheng31@gmail.com>
2013-04-17 16:46:41 +0800

    Basic FolderViewController appearance.

--- arc/arc/Controllers/FolderViewController.m
+++ arc/arc/Controllers/FolderViewController.m
@@ -87,66 +87,107 @@
 
 - (NSInteger)numberOfSectionsInTableView:(UITableView *)tableView
 {
-#warning Potentially incomplete method implementation.
     // Return the number of sections.
-    return 0;
+    return [_segregatedContents count];
 }
 
 - (NSInteger)tableView:(UITableView *)tableView numberOfRowsInSection:(NSInteger)section
 {
-#warning Incomplete method implementation.
     // Return the number of rows in the section.
-    return 0;
+    return [[_segregatedContents objectAtIndex:section] count];
+}
+
+//- (NSString *)tableView:(UITableView *)tableView titleForHeaderInSection:(NSInteger)section {
+//    // Don't display a title if the section is empty.
+//    if ([self tableView:tableView numberOfRowsInSection:section] == 0) {
+//        return nil;
+//    }
+//    // Returns the header for the given section.
+//    return section == 0 ? @"Folders" : @"Files";
+//}
+//
+//- (CGFloat)tableView:(UITableView *)tableView heightForHeaderInSection:(NSInteger)section
+//{
+//    // Don't display the header if the section has no rows.
+//    if ([self tableView:tableView numberOfRowsInSection:section] == 0) {
+//        return 0;
+//    }
+//    // Return the height of the header for the given section.
+//    return 22;
+//}
+
+- (UIView*)tableView:(UITableView *)tableView viewForHeaderInSection:(NSInteger)section
+{
+    // Create a custom view for the section headers.
+    UIView *customView = [[UIView alloc] initWithFrame:CGRectMake(10.0, 0.0, 320.0, 22.0)];
+    customView.backgroundColor = [Utils colorWithHexString:@"CC272821"];
+    
+    UILabel *headerLabel = [[UILabel alloc] initWithFrame:CGRectZero];
+    headerLabel.backgroundColor = [UIColor clearColor];
+    headerLabel.opaque = NO;
+    headerLabel.textColor = [UIColor whiteColor];
+    headerLabel.font = [UIFont fontWithName:@"Helvetica Neue Bold" size:18];
+    headerLabel.shadowOffset = CGSizeMake(0.0f, 1.0f);
+    headerLabel.shadowColor = [UIColor colorWithRed:0.0 green:0.0 blue:0.0 alpha:0.5];
+    headerLabel.frame = CGRectMake(11,-11, 320.0, 44.0);
+    headerLabel.textAlignment = NSTextAlignmentLeft;
+    headerLabel.text = section == 0 ? @"Folders" : @"Files";
+    [customView addSubview:headerLabel];
+    return customView;
 }
 
 - (UITableViewCell *)tableView:(UITableView *)tableView cellForRowAtIndexPath:(NSIndexPath *)indexPath
 {
-    static NSString *CellIdentifier = @"Cell";
-    UITableViewCell *cell = [tableView dequeueReusableCellWithIdentifier:CellIdentifier forIndexPath:indexPath];
+    static NSString *cellIdentifier = @"FileAndFolderCell";
+    UITableViewCell *cell = [tableView dequeueReusableCellWithIdentifier:cellIdentifier];
     
-    // Configure the cell...
+    if (cell == nil) {
+        cell = [[UITableViewCell alloc] initWithStyle:UITableViewCellStyleSubtitle
+                                      reuseIdentifier:cellIdentifier];
+    }
+    
+    
+    NSArray *section = [_segregatedContents objectAtIndex:[indexPath section]];
+    id<FileSystemObject> fileObject = [section objectAtIndex:[indexPath row]];
+    
+    NSString *detailDescription;
+    UIImage *cellImage;
+    if ([[fileObject class] conformsToProtocol:@protocol(File)]) {
+        cellImage = [Utils scale:[UIImage imageNamed:@"file.png"]
+                          toSize:CGSizeMake(35, 35)];
+        detailDescription = [Utils humanReadableFileSize:[fileObject size]];
+    } else if ([[fileObject class] conformsToProtocol:@protocol(Folder)]) {
+        cellImage = [Utils scale:[UIImage imageNamed:@"folder.png"]
+                          toSize:CGSizeMake(35, 35)];
+        
+        switch ((int)[fileObject size]) {
+            case 0:
+                detailDescription = @"Empty Folder";
+                break;
+            case 1:
+                detailDescription = @"1 item";
+            default:
+                detailDescription = [NSString stringWithFormat:@"%d items", (int)[fileObject size]];
+                break;
+        }
+    }
+    
+    // Cell Text Appearance
+    cell.textLabel.text = fileObject.name;
+    cell.textLabel.font = [UIFont fontWithName:@"Helvetica Neue" size:17];
+    cell.detailTextLabel.text = detailDescription;
+    cell.detailTextLabel.font = [UIFont fontWithName:@"Helvetica Neue" size:12];
+    
+    cell.imageView.image = cellImage;
+    
+    // Selection Appearance
+    UIView *backgroundView = [[UIView alloc] init];
+    backgroundView.backgroundColor = [Utils colorWithHexString:@"ee151512"];
+    cell.selectedBackgroundView = backgroundView;
     
     return cell;
 }
 
-/*
-// Override to support conditional editing of the table view.
-- (BOOL)tableView:(UITableView *)tableView canEditRowAtIndexPath:(NSIndexPath *)indexPath
-{
-    // Return NO if you do not want the specified item to be editable.
-    return YES;
-}
-*/
-
-/*
-// Override to support editing the table view.
-- (void)tableView:(UITableView *)tableView commitEditingStyle:(UITableViewCellEditingStyle)editingStyle forRowAtIndexPath:(NSIndexPath *)indexPath
-{
-    if (editingStyle == UITableViewCellEditingStyleDelete) {
-        // Delete the row from the data source
-        [tableView deleteRowsAtIndexPaths:@[indexPath] withRowAnimation:UITableViewRowAnimationFade];
-    }   
-    else if (editingStyle == UITableViewCellEditingStyleInsert) {
-        // Create a new instance of the appropriate class, insert it into the array, and add a new row to the table view
-    }   
-}
-*/
-
-/*
-// Override to support rearranging the table view.
-- (void)tableView:(UITableView *)tableView moveRowAtIndexPath:(NSIndexPath *)fromIndexPath toIndexPath:(NSIndexPath *)toIndexPath
-{
-}
-*/
-
-/*
-// Override to support conditional rearranging of the table view.
-- (BOOL)tableView:(UITableView *)tableView canMoveRowAtIndexPath:(NSIndexPath *)indexPath
-{
-    // Return NO if you do not want the item to be re-orderable.
-    return YES;
-}
-*/
 
 #pragma mark - Table view delegate
 
