c0d5a95745e15c8f215a66445cca5e05e46e0c10
Jerome Cheng <jcheng31@gmail.com>
2013-04-17 22:41:14 +0800

    Add methods to notify FolderView delegate when edit mode is toggled.

--- arc/arc/Controllers/Protocols/FolderViewControllerDelegate.h
+++ arc/arc/Controllers/Protocols/FolderViewControllerDelegate.h
@@ -14,8 +14,13 @@
 
 @protocol FolderViewControllerDelegate <NSObject>
 
+// Allows delegate to know which file or folder was selected in navigation mode.
 - (void)folderViewController:(FolderViewController *)sender selectedFile:(id<File>)file;
-
 - (void)folderViewController:(FolderViewController *)sender selectedFolder:(id<Folder>)folder;
+
+// Allows the delegate to know if the controller has entered or left editing mode.
+- (void)folderViewController:(FolderViewController *)folderviewController
+     DidEnterEditModeAnimate:(BOOL)animate;
+- (void)folderViewController:(FolderViewController *)folderviewController DidExitEditModeAnimate:(BOOL)animate;
 
 @end
