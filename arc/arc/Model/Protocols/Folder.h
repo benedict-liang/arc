c81d17dc84952cf648e00da39eabb32274d9b0ef
Jerome Cheng <jcheng31@gmail.com>
2013-04-19 11:18:48 +0800

    Make Folder methods optional.

--- arc/arc/Model/Protocols/Folder.h
+++ arc/arc/Model/Protocols/Folder.h
@@ -10,7 +10,7 @@
 #import "FileSystemObject.h"
 @protocol Folder <FileSystemObject>
 
-@required
+@optional
 // Moves the given FileSystemObject to this Folder.
 // The given file must be of the same "type" as this Folder
 // (e.g. iOS file system, DropBox, etc.)
