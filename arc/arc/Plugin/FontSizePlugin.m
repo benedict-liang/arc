b09e5c8b5230daa4716368158b7e6dd6ec9ce7bd
Benedict Liang <benedictljj@gmail.com>
2013-04-19 17:34:15 +0800

    Changed font size settings view to MCQ.

--- arc/arc/Plugin/FontSizePlugin.m
+++ arc/arc/Plugin/FontSizePlugin.m
@@ -18,15 +18,66 @@
 
 - (id)init
 {
+//    self = [super init];
+//    if (self) {
+//        _setting = @"fontSize";
+//        _defaultFontSize = 14;
+//        _properties = @{
+//                        PLUGIN_TITLE: @"Font Size",
+//                        PLUGIN_TYPE: [NSNumber numberWithInt:kRangeSettingType],
+//                        PLUGIN_OPTION_RANGE_MIN: [NSNumber numberWithInt:10],
+//                        PLUGIN_OPTION_RANGE_MAX: [NSNumber numberWithInt:70]
+//                        };
+//    }
+//    return self;
+    
     self = [super init];
     if (self) {
         _setting = @"fontSize";
         _defaultFontSize = 14;
+        // Setup the dictionary to be returned.
         _properties = @{
                         PLUGIN_TITLE: @"Font Size",
-                        PLUGIN_TYPE: [NSNumber numberWithInt:kRangeSettingType],
-                        PLUGIN_OPTION_RANGE_MIN: [NSNumber numberWithInt:10],
-                        PLUGIN_OPTION_RANGE_MAX: [NSNumber numberWithInt:70]
+                        PLUGIN_TYPE: [NSNumber numberWithInt:kMCQSettingType],
+                        PLUGIN_OPTIONS: @[
+                                @{
+                                    PLUGIN_OPTION_LABEL: @"Smallest",
+                                    PLUGIN_OPTION_VALUE: @"Smallest"
+                                    },
+
+                                @{
+                                    PLUGIN_OPTION_LABEL: @"Tiny",
+                                    PLUGIN_OPTION_VALUE: @"Tiny"
+                                    },
+                                @{
+                                    PLUGIN_OPTION_LABEL: @"Smaller",
+                                    PLUGIN_OPTION_VALUE: @"Smaller"
+                                    },
+                                @{
+                                    PLUGIN_OPTION_LABEL: @"Small",
+                                    PLUGIN_OPTION_VALUE: @"Small"
+                                    },
+                                @{
+                                    PLUGIN_OPTION_LABEL: @"Medium",
+                                    PLUGIN_OPTION_VALUE: @"14"
+                                    },
+                                @{
+                                    PLUGIN_OPTION_LABEL: @"Large",
+                                    PLUGIN_OPTION_VALUE: @"Large"
+                                    },
+                                @{
+                                    PLUGIN_OPTION_LABEL: @"Larger",
+                                    PLUGIN_OPTION_VALUE: @"Larger"
+                                    },
+                                @{
+                                    PLUGIN_OPTION_LABEL: @"Giant",
+                                    PLUGIN_OPTION_VALUE: @"Giant"
+                                    },
+                                @{
+                                    PLUGIN_OPTION_LABEL: @"Largest",
+                                    PLUGIN_OPTION_VALUE: @"Largest"
+                                    }
+                                ]
                         };
     }
     return self;
