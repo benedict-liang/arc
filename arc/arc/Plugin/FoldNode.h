134053873a5843e090f549b5508eda260913ca4c
Michael Yong <wrong92@gmail.com>
2013-04-20 19:13:57 +0800

    clean foldnode

--- arc/arc/Plugin/FoldNode.h
+++ arc/arc/Plugin/FoldNode.h
@@ -7,17 +7,21 @@
 //
 
 #import <Foundation/Foundation.h>
-typedef enum {kRootNode, kChildNode} FoldNodeType;
-@interface FoldNode : NSObject <NSCopying>
-@property(readonly) NSRange contentRange;
-@property(readonly) NSRange startRange;
-@property(readonly) NSRange endRange;
-@property FoldNodeType type;
 
--(id)initWithContentRange:(NSRange)cr
+typedef enum {
+    kRootNode,
+    kChildNode
+} FoldNodeType;
+
+@interface FoldNode : NSObject <NSCopying>
+@property (nonatomic, readonly) NSRange contentRange;
+@property (nonatomic, readonly) NSRange startRange;
+@property (nonatomic, readonly) NSRange endRange;
+@property (nonatomic) FoldNodeType type;
+
+- (id)initWithContentRange:(NSRange)cr
                startRange:(NSRange)sr
                  endRange:(NSRange)er;
-
--(id)initWithNode:(FoldNode*)node;
-+ (NSArray*)sortNodeArray:(NSArray*)nodes;
+- (id)initWithNode:(FoldNode *)node;
++ (NSArray *)sortNodeArray:(NSArray *)nodes;
 @end
