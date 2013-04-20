ccd6362ecc19afda110f923e97e70f3b2ab9e0f0
Jerome Cheng <jcheng31@gmail.com>
2013-04-20 17:46:46 +0800

    Properly handle paths with spaces in them.

--- arc/arc/Model/LocalFile.m
+++ arc/arc/Model/LocalFile.m
@@ -15,14 +15,14 @@
 @implementation LocalFile
 
 // Synthesize properties from protocol.
-@synthesize name=_name, identifier=_path, parent=_parent, extension=_extension, isRemovable=_isRemovable, size=_size, lastModified=_lastModified;
+@synthesize name=_name, identifier=_identifier, parent=_parent, extension=_extension, isRemovable=_isRemovable, size=_size, lastModified=_lastModified;
 
 // Initialises this object with the given name, path, and parent.
-- (id)initWithName:(NSString *)name identifier:(NSString *)path parent:(id<FileSystemObject>)parent
+- (id)initWithName:(NSString *)name identifier:(NSString *)identifier parent:(id<FileSystemObject>)parent
 {
     if (self = [super init]) {
         _name = name;
-        _path = path;
+        _identifier = identifier;
         _parent = parent;
         _extension = [name pathExtension];
         _isRemovable = YES;
@@ -37,7 +37,7 @@
     NSFileManager *fileManager = [NSFileManager defaultManager];
     
     NSError *error;
-    NSDictionary *fileProperties = [fileManager attributesOfItemAtPath:_path error:&error];
+    NSDictionary *fileProperties = [fileManager attributesOfItemAtPath:_identifier error:&error];
     
     if (error) {
         NSLog(@"%@", error);
@@ -51,7 +51,7 @@
 - (NSString *)contents
 {
     NSError *error;
-    NSString *retrievedContents = [NSString stringWithContentsOfFile:_path encoding:NSUTF8StringEncoding error:&error];
+    NSString *retrievedContents = [NSString stringWithContentsOfFile:_identifier encoding:NSUTF8StringEncoding error:&error];
     
     if (error) {
         NSLog(@"%@", error);
@@ -69,7 +69,7 @@
     NSFileManager *fileManager = [NSFileManager defaultManager];
     
     NSError *error;
-    BOOL isRemoveSuccessful = [fileManager removeItemAtPath:_path error:&error];
+    BOOL isRemoveSuccessful = [fileManager removeItemAtPath:_identifier error:&error];
     
     if (!isRemoveSuccessful) {
         NSLog(@"%@", error);
