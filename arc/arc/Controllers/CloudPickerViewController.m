1e5d5e45a87c01c87ab5cec7ebb26dc8225f6333
Jerome Cheng <jcheng31@gmail.com>
2013-04-20 12:33:02 +0800

    Copy across section header appearance. (To be abstracted.)

--- arc/arc/Controllers/CloudPickerViewController.m
+++ arc/arc/Controllers/CloudPickerViewController.m
@@ -115,13 +115,23 @@
 }
 
 // Returns the header for the given section.
-- (NSString *)tableView:(UITableView *)tableView titleForHeaderInSection:(NSInteger)section
+- (UIView*)tableView:(UITableView *)tableView viewForHeaderInSection:(NSInteger)section
 {
-    // Hide section title if section has zero rows
-    if ([self tableView:tableView numberOfRowsInSection:section] == 0) {
-        return nil;
-    }
-    return section == 0 ? @"Folders" : @"Files";
+    UIView *customView = [[UIView alloc] initWithFrame:CGRectMake(10.0, 0.0, 320.0, 22.0)];
+    customView.backgroundColor = [Utils colorWithHexString:@"CC272821"];
+    
+    UILabel *headerLabel = [[UILabel alloc] initWithFrame:CGRectZero];
+    headerLabel.backgroundColor = [UIColor clearColor];
+    headerLabel.opaque = NO;
+    headerLabel.textColor = [UIColor whiteColor];
+    headerLabel.font = [UIFont fontWithName:@"Helvetica Neue Bold" size:18];
+    headerLabel.shadowOffset = CGSizeMake(0.0f, 1.0f);
+    headerLabel.shadowColor = [UIColor colorWithRed:0.0 green:0.0 blue:0.0 alpha:0.5];
+    headerLabel.frame = CGRectMake(11,-11, 320.0, 44.0);
+    headerLabel.textAlignment = NSTextAlignmentLeft;
+    headerLabel.text = section == 0 ? @"Folders" : @"Files";
+    [customView addSubview:headerLabel];
+    return customView;
 }
 
 // Set up the cell at the given index path.
