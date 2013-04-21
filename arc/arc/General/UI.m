b15e062de89adfa58a8c4ff975e65296c4583f2e
Michael Yong <wrong92@gmail.com>
2013-04-21 22:08:06 +0800

    more refactoring

--- arc/arc/General/UI.m
+++ arc/arc/General/UI.m
@@ -11,10 +11,27 @@
 @implementation UI
 + (void)exec
 {
+    [UI navigationBarAppearance];
+    [UI barButtonItemAppearance];
+    [UI toolBarAppearance];
+}
+
++ (void)navigationBarAppearance
+{
     [[UINavigationBar appearance] setBackgroundImage: [Utils imageWithColor:
                                                        [Utils colorWithHexString:@"272821"]]
                                        forBarMetrics:UIBarMetricsDefault];
-    
+    // NavBar title
+    [[UINavigationBar appearance] setTitleTextAttributes:[NSDictionary dictionaryWithObjectsAndKeys:
+                                                          [UIColor whiteColor], UITextAttributeTextColor,
+                                                          [UIColor colorWithRed:0 green:0 blue:0 alpha:0], UITextAttributeTextShadowColor,
+                                                          [NSValue valueWithUIOffset:UIOffsetMake(0, 0)], UITextAttributeTextShadowOffset,
+                                                          [UIFont fontWithName:@"Helvetica Neue" size:0.0], UITextAttributeFont,
+                                                          nil]];
+}
+
++ (void)barButtonItemAppearance
+{
     [[UIBarButtonItem appearance] setBackgroundImage:[Utils imageWithColor:[Utils colorWithHexString:@"151512"]]
                                             forState:UIControlStateNormal
                                           barMetrics:UIBarMetricsDefault];
@@ -28,13 +45,6 @@
                                                           nil]
                                                 forState:UIControlStateNormal];
     
-    // NavBar title
-    [[UINavigationBar appearance] setTitleTextAttributes:[NSDictionary dictionaryWithObjectsAndKeys:
-                                                          [UIColor whiteColor], UITextAttributeTextColor,
-                                                          [UIColor colorWithRed:0 green:0 blue:0 alpha:0], UITextAttributeTextShadowColor,
-                                                          [NSValue valueWithUIOffset:UIOffsetMake(0, 0)], UITextAttributeTextShadowOffset,
-                                                          [UIFont fontWithName:@"Helvetica Neue" size:0.0], UITextAttributeFont,
-                                                          nil]];
     // TODO
     // make back button arrow shaped.
     [[UIBarButtonItem appearance] setBackButtonBackgroundImage:[[Utils imageSized:CGRectMake(0, 0, 36, 36)
@@ -42,10 +52,14 @@
                                                                 resizableImageWithCapInsets:UIEdgeInsetsMake(0, 16, 0, 16)]
                                                       forState:UIControlStateNormal
                                                     barMetrics:UIBarMetricsDefault];
-    
+}
+
++ (void)toolBarAppearance
+{
     [[UIToolbar appearance] setBackgroundImage:[Utils imageWithColor:
                                                 [Utils colorWithHexString:@"191919"]]
                             forToolbarPosition:UIToolbarPositionAny
                                     barMetrics:UIBarMetricsDefault];
 }
+
 @end
