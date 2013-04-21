520a7ff172563cf7b7df1b213f9f489fa73d490d
Michael Yong <wrong92@gmail.com>
2013-04-21 16:03:07 +0800

    Added destionation folder View controller back

---
+++ arc/arc/Controllers/FolderViewController/DestinationFolderViewController.h
@@ -0,0 +1,13 @@
+//
+//  DestinationFolderViewController.h
+//  arc
+//
+//  Created by Yong Michael on 21/4/13.
+//  Copyright (c) 2013 nus.cs3217. All rights reserved.
+//
+
+#import "BasicFolderViewController.h"
+
+@interface DestinationFolderViewController : BasicFolderViewController<UITableViewDataSource, UITableViewDelegate>
+//@property (nonatomic, weak) id<Folder
+@end
