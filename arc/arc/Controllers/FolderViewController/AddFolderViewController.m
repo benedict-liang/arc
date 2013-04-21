99a45da0b6aaf125fe0eb4d185aa3d583f51de91
Michael Yong <wrong92@gmail.com>
2013-04-21 23:00:00 +0800

    WIP add folder modal

--- arc/arc/Controllers/FolderViewController/AddFolderViewController.m
+++ arc/arc/Controllers/FolderViewController/AddFolderViewController.m
@@ -9,7 +9,7 @@
 #import "AddFolderViewController.h"
 
 @interface AddFolderViewController ()
-
+@property (nonatomic, strong) UITextField *textField;
 @end
 
 @implementation AddFolderViewController
@@ -28,13 +28,46 @@
 {
     [super viewDidLoad];
     self.title = @"Create a Folder";
+    self.view.autoresizingMask = UIViewAutoresizingFlexibleWidth;
+    self.view.autoresizesSubviews = YES;
     
     UIBarButtonItem *cancelButton =
     [[UIBarButtonItem alloc] initWithTitle:@"Cancel"
                                      style:UIBarButtonItemStyleBordered
                                     target:self
                                     action:@selector(cancel:)];
-    self.navigationItem.rightBarButtonItem = cancelButton;
+    
+    UIBarButtonItem *createFolder =
+    [[UIBarButtonItem alloc] initWithTitle:@"Create Folder"
+                                     style:UIBarButtonItemStyleBordered
+                                    target:self
+                                    action:@selector(createFolder:)];
+    
+    self.navigationItem.leftBarButtonItem = cancelButton;
+    self.navigationItem.rightBarButtonItem = createFolder;
+    
+    UIView *container = [[UIView alloc] init];
+    container.autoresizingMask = UIViewAutoresizingFlexibleWidth;
+    container.backgroundColor = [UIColor whiteColor];
+    container.frame = CGRectMake(10, 10, self.view.frame.size.width - 20, 50);
+    [self.view addSubview:container];
+    
+    _textField = [[UITextField alloc]
+                  initWithFrame:CGRectMake(10, 10, floorf((container.frame.size.width - 20)/2), 30)];
+    _textField.adjustsFontSizeToFitWidth = YES;
+    _textField.font = [UIFont fontWithName:[UI fontName] size:20];
+    _textField.textColor = [UIColor blackColor];
+    _textField.placeholder = @"Untitled Folder";
+    _textField.keyboardType = UIKeyboardTypeAlphabet;
+    _textField.returnKeyType = UIReturnKeyDone;
+    _textField.backgroundColor = [UIColor whiteColor];
+    _textField.autocapitalizationType = UITextAutocapitalizationTypeNone;
+    _textField.autocorrectionType = UITextAutocorrectionTypeNo;
+    _textField.textAlignment = NSTextAlignmentLeft;
+    _textField.enabled = YES;
+    [container addSubview:_textField];
+
+    [_textField becomeFirstResponder];
 }
 
 - (void)cancel:(id)sender
@@ -42,6 +75,15 @@
     [self.delegate modalViewControllerDone:
      [FolderCommandObject commandOfType:kCancelCommand
                              withTarget:nil]];
+}
+
+- (void)createFolder:(id)sender
+{
+    if ([_textField text].length != 0) {
+        [self.delegate modalViewControllerDone:
+         [FolderCommandObject commandOfType:kCreateFolderCommand
+                                 withTarget:[_textField text]]];
+    }
 }
 
 @end
--- arc/arc/Controllers/FolderViewController/FolderCommandObject.h
+++ arc/arc/Controllers/FolderViewController/FolderCommandObject.h
@@ -11,6 +11,7 @@
 
 typedef enum {
     kMoveFileObjects,
+    kCreateFolderCommand,
     kCancelCommand
 } kFolderCommandType;
 
